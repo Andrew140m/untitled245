70c6da1ebe5fdcaeee036a04545c988853cf7a2a
Pus9vGang <79223957+Andrew140m@users.noreply.github.com>
2022-01-17 17:38:21 +0300

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,16 +1,61 @@
-#include <iostream>
-typedef unsigned long long ull;
+#include <bits/stdc++.h>
 using namespace std;
-ull rem(ull x, ull y, ull m) {
-	if (x == m - 1 || y == m - 1) return 0;
-	if (x + y < m) return 1;
-	return (x + y + 2 - m);
-}
-int main() {
-	int t; cin >> t;
-	for (int test = 0; test < t; test++) {
-		ull n, a, b;
-		cin >> n >> a >> b;
-		cout << n*((a+1)/n)*((b+1)/n) + ((a+1)/n)*((b+1)%n) + ((b+1)/n)*((a+1)%n) + rem(a % n, b % n, n) - 1 <<endl;
-	}
+
+#define ll long long
+#define ull unsigned long long
+#define vi vector <int>
+#define vsi vector <short int>
+#define vll vector <long long>
+#define Hacks ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define Sort(vector) (sort(vector.begin(), vector.end()))
+#define Cin(vector, quantity) for(decltype(quantity)& quantity : vector) {cin >> quantity;}
+#define Cin1(vector, quantity) for(decltype(quantity) i=0; i<quantity; i++) {cin>>vector[i];}
+#define Cout(vector, quantity) for(decltype(quantity) i=0; i<quantity; i++) {cout<<vector[i]<<endl;}
+#define Cout1(vector, quantity) for(decltype(quantity) i=0; i<quantity; i++) {cout<<vector[i]<<" ";}
+#define forn(x, n) for(decltype(n) x = 0; x < n; ++ x)
+#define fornon(x, n) for(decltype(n) x = 1; x <= n; ++ x)
+#define For(left, right) for(decltype(left) i = left; i <= right; ++ i)
+#define endl '\n'
+#define yeS cout << "YES" << endl;
+#define nO cout << "NO" << endl;
+#define _ << " " <<
+ 
+/*
+4
+4 2 2
+4 4 4
+6 5 5
+8 13 17
+*/
+ 
+signed main() {
+    Hacks;
+    int n, t, z, g, a, b;
+    cin >> t;
+    while (t--) {
+        ll var = 0;
+        cin >> n >> a >> b;
+        int v = n;
+        if (a == 0) {
+            while (b >= v) {
+                var += 1; v += n;
+            }
+            cout << var << endl;
+        }
+        else {
+            if (a < b) {
+                swap(a, b);
+            }
+            while (a + b >= v) {
+                g = min(v, a);
+                z = abs(v - g);
+                while (g <= a && min(v, b) >= z) {
+                    var += 1;
+                    g--; z++;
+                }
+                v += n;
+            }
+            cout << var << endl;
+        }
+    }
 }
